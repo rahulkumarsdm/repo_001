eeb18cd80cc44472dff99b171a3f5f3876eafdf8
rahulkumarsdm <rahul.kumar.sdm@gmail.com>
2018-07-13 16:31:40 +0530

    Second Commit - Project001

---
+++ JSPractice/Project001/script.js
@@ -0,0 +1,29 @@
+console.log("Hello World!!!");
+
+let name = 'MyName';
+console.log(name);
+
+var lastName = 'MyLastName';
+console.log(lastName);
+
+console.log(name + ' ' + lastName);
+
+var age = 27;
+console.log(age);
+
+// Data Types in JS
+Number
+String
+Boolean
+undefined
+null
+// JS uses dynamic typing = The type is assigned at the time of runtime
+
+//  Hoisting Example
+x = 10;
+y = 20;
+console.log(x + ' ' + y);
+var x,y;
+
+// Variable Mutation and Type Coercion
+
